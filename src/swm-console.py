d25c3a0860a73c8c78375695d828aca624737092
Taras Shapovalov <taras@iclouds.net>
2021-11-28 20:03:43 +0300

    Format

--- src/swm-console.py
+++ src/swm-console.py
@@ -15,14 +15,14 @@ def main() -> None:
     parser = argparse.ArgumentParser(description="Sky Port terminal implemented as a console script.")
     group = parser.add_mutually_exclusive_group(required=True)
 
-    parser.add_argument("--no-header", help="Do not print header in tables", action='store_true')
+    parser.add_argument("--no-header", help="Do not print header in tables", action="store_true")
 
     group.add_argument("--job-info", help="Show single job details")
     group.add_argument("--job-submit", help="Submit a new job script")
-    group.add_argument("--job-list", help="Show all jobs", action='store_true')
-    group.add_argument("--remote-list", help="Show remote sites", action='store_true')
-    group.add_argument("--node-list", help="Show nodes", action='store_true')
-    group.add_argument("--flavor-list", help="Show available flavors", action='store_true')
+    group.add_argument("--job-list", help="Show all jobs", action="store_true")
+    group.add_argument("--remote-list", help="Show remote sites", action="store_true")
+    group.add_argument("--node-list", help="Show nodes", action="store_true")
+    group.add_argument("--flavor-list", help="Show available flavors", action="store_true")
 
     args = parser.parse_args()
 
@@ -50,13 +50,14 @@ def main() -> None:
 def print_job_info(args: argparse.Namespace, swm_api: SwmApi) -> None:
     job_id = args.job_info
     if (job := swm_api.get_job(job_id)) is not None:
-        table = [["ID", job.id],
-                 ["Name", job.name],
-                 ["State", job.state],
-                 ["Submit", job.submit_time],
-                 ["Start", job.start_time],
-                 ["End", job.end_time],
-                 ["Nodes", ", ".join(job.node_names)],
+        table = [
+            ["ID", job.id],
+            ["Name", job.name],
+            ["State", job.state],
+            ["Submit", job.submit_time],
+            ["Start", job.start_time],
+            ["End", job.end_time],
+            ["Nodes", ", ".join(job.node_names)],
         ]
         print(tabulate(table, tablefmt="presto"))
     else:
@@ -84,7 +85,7 @@ def find_resource(name: str, resources: typing.List[Resource]) -> typing.Optiona
 
 def get_res_storage(resources: typing.List[Resource]) -> str:
     if (res := find_resource("storage", resources)) is not None:
-        if res.count >= 1000*1000:
+        if res.count >= 1000 * 1000:
             return str(int(res.count / (1000 * 1000 * 1000))) + " GB"
         return str(res.count) + " B"
     return ""
@@ -92,7 +93,7 @@ def get_res_storage(resources: typing.List[Resource]) -> str:
 
 def get_res_mem(resources: typing.List[Resource]) -> str:
     if (res := find_resource("mem", resources)) is not None:
-        if res.count >= 1000*1000:
+        if res.count >= 1000 * 1000:
             return str(int(res.count / (1000 * 1000))) + " MB"
         return str(res.count) + " B"
     return ""
@@ -110,14 +111,17 @@ def print_nodes(args: argparse.Namespace, swm_api: SwmApi) -> None:
         headers = [] if args.no_header else ["ID", "Name", "Power", "Alloc", "Storage", "Mem", "CPUs"]
         table = []
         for node in nodes:
-            table.append([node.id,
-                          node.name,
-                          node.state_power,
-                          node.state_alloc,
-                          get_res_storage(node.resources),
-                          get_res_mem(node.resources),
-                          get_res_cpus(node.resources),
-                         ])
+            table.append(
+                [
+                    node.id,
+                    node.name,
+                    node.state_power,
+                    node.state_alloc,
+                    get_res_storage(node.resources),
+                    get_res_mem(node.resources),
+                    get_res_cpus(node.resources),
+                ]
+            )
         print(tabulate(table, headers=headers, tablefmt="presto"))
     else:
         print(f"Wrong output: {nodes}", file=sys.stderr)
@@ -130,12 +134,15 @@ def print_remote_sites(args: argparse.Namespace, swm_api: SwmApi) -> None:
         headers = [] if args.no_header else ["ID", "Name", "Kind", "Default image ID", "Default flavor ID"]
         table = []
         for remote in remotes:
-            table.append([remote.id,
-                          remote.name,
-                          remote.kind,
-                          remote.default_image_id,
-                          remote.default_flavor_id,
-                         ])
+            table.append(
+                [
+                    remote.id,
+                    remote.name,
+                    remote.kind,
+                    remote.default_image_id,
+                    remote.default_flavor_id,
+                ]
+            )
         print(tabulate(table, headers=headers, tablefmt="presto"))
     else:
         print(f"Wrong output: {remotes}", file=sys.stderr)
@@ -148,14 +155,17 @@ def print_jobs(args: argparse.Namespace, swm_api: SwmApi) -> None:
         headers = [] if args.no_header else ["ID", "Name", "State", "Submit time", "Start time", "End time", "Nodes"]
         table = []
         for job in jobs:
-            table.append([job.id,
-                          job.name,
-                          job.state,
-                          job.submit_time,
-                          job.start_time,
-                          job.end_time,
-                          ", ".join(job.node_names),
-                         ])
+            table.append(
+                [
+                    job.id,
+                    job.name,
+                    job.state,
+                    job.submit_time,
+                    job.start_time,
+                    job.end_time,
+                    ", ".join(job.node_names),
+                ]
+            )
         print(tabulate(table, headers=headers, tablefmt="presto"))
     else:
         print(f"Wrong output: {jobs}", file=sys.stderr)
@@ -168,13 +178,16 @@ def print_flavors(args: argparse.Namespace, swm_api: SwmApi) -> None:
         headers = [] if args.no_header else ["ID", "Name", "Storage", "Mem", "CPUs", "Price"]
         table = []
         for flavor in flavors:
-            table.append([flavor.id,
-                          flavor.name,
-                          get_res_storage(flavor.resources),
-                          get_res_mem(flavor.resources),
-                          get_res_cpus(flavor.resources),
-                          flavor.price,
-                         ])
+            table.append(
+                [
+                    flavor.id,
+                    flavor.name,
+                    get_res_storage(flavor.resources),
+                    get_res_mem(flavor.resources),
+                    get_res_cpus(flavor.resources),
+                    flavor.price,
+                ]
+            )
         print(tabulate(table, headers=headers, tablefmt="presto"))
     else:
         print(f"Wrong output: {flavors}", file=sys.stderr)
